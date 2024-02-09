7d11722f089c3bcbeb0090f5c5103b00c2c02c73
k9956245 <89276559+k9956245@users.noreply.github.com>
2024-02-09 10:36:38 +0000

    Chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 # Register your models here.
+
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
@@ -13,3 +15,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
+
--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -21,6 +24,7 @@ class PageForm(forms.ModelForm):
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+
     class Meta:
         model = Page
         exclude = ('category',)
@@ -35,3 +39,21 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    
+
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
